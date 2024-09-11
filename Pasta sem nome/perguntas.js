f90ba2718c21dda7a768f77c4f8164b5a80c725f
tamiresferreirapin <128374859+tamiresferreirapin@users.noreply.github.com>
2024-09-11 11:14:33 -0300

    Add files via upload

---
+++ Pasta sem nome/perguntas.js
@@ -0,0 +1,98 @@
+export const perguntas = [
+    {
+        enunciado: "Primeiro. Você é a favor da legalização?",
+        alternativas: [
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Você se pronuncia publícamente que não é a favor do uso da maconha."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Você se pronuncia publícamente que é a favor do uso da maconha."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O que seria bom para reduzir o uso da erva?",
+        alternativas: [
+            {
+                texto: "Manter a situação",
+                afirmacao: [
+                    "Porem, seria muito dificil soucionar o problema com ela, e escolheu simplismente deixar quieto."
+                ],
+            },
+            {
+                texto: "Varrer a maconha do país",
+                afirmacao: [
+                    "Seu maior ato foi mover o exercito para as favelas e eliminar qualquer resquicio de que já teve canabis nesse país."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Mas a maconha ainda não foi legalizada. Como vamos legalizar ela?",
+        alternativas: [
+            {
+                texto: "Propor a liberação no STF.",
+                afirmacao: [
+                    "Assim que anuncia, propoem uma lei no STF, que perguntão por um motivo bom para acitarem.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não precisa.",
+                afirmacao: [
+                    "Entre tanto, diz ser melhor não mudar nada da cituação atual, e que vai ser melhor assim.",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "O STF perguntão por um motivo bom para acitarem, e você diz...",
+        alternativas: [
+            {
+                texto: "É direito das pessoas por não prejudicar a ninguem.",
+                afirmacao: [
+                    "Seus motivos para liberar foram relevante, e o STF aceita o pedido.",
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Eles sabem oque fazem.",
+                afirmacao: [
+                    "Mas seu motivo foi muito inrrelevante, que leva a uma votação para te tirar o poder, e você volta a ser CLT.",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Seus motivos para liberar foram relevante, e o STF aceita o pedido.",
+        alternativas: [
+            {
+                texto: "ver oq vai dar",
+                afirmacao: [
+                    "Mas o país usa sem moderação, oque leva o Brasil a um munde de vicios."
+                ],
+                proxima: 5,
+            },
+    
+        ]
+    },
+    {
+        enunciado: "Mas o país usa sem moderação, oque leva o Brasil a um munde de vicios.",
+        alternativas: [
+            {
+                texto: "Desistir",
+                afirmacao: [
+                    "Você desiste da pilitíca, e volta a ser CLT em um mundo de viciados."
+                ],
+            }
+        ]
+    },
+];
